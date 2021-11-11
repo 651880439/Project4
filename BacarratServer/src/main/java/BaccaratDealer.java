1caef73ae4e3e23e1521d3966792daded0ef46bc
IMFeeferz <47335443+IMFeeferz@users.noreply.github.com>
2021-11-11 17:11:18 -0600

    Add files via upload

--- BacarratServer/src/main/java/BaccaratDealer.java
+++ BacarratServer/src/main/java/BaccaratDealer.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.*;
 
 public class BaccaratDealer {
 	ArrayList<Card> deck = new ArrayList<Card>();
@@ -25,16 +26,25 @@ public class BaccaratDealer {
 	
 	public ArrayList<Card> dealHand(){
 		ArrayList<Card> cardList = new ArrayList<Card>();
+		Card myCard1, myCard2;
+		myCard1 = deck.get(0);
+		myCard2 = deck.get(1);
+		deck.remove(0);
+		deck.remove(1);
+		cardList.add(myCard1);
+		cardList.add(myCard2);
 		return cardList;	
 	}
 	
 	public Card drawOne() {
 		Card myCard = new Card("", 0);
+		myCard = deck.get(0);
+		deck.remove(0);
 		return myCard;
 	}
 	
 	public void shuffleDeck() {
-		
+		Collections.shuffle(deck);
 	}
 	
 	public int deckSize() {
--- BacarratServer/src/main/java/BaccaratGameLogic.java
+++ BacarratServer/src/main/java/BaccaratGameLogic.java
@@ -7,19 +7,46 @@ public class BaccaratGameLogic {
 	
 	public static int handTotal(ArrayList<Card> hand) {
 		return hand.size();
-		
 	}
 	
 	public static boolean evaluateBankerDraw(ArrayList<Card> hand, Card playerCard) {
-		for(int i = 0; i < hand.size(); i++) {
-			if(hand.get(i) == playerCard) {
+		int bankerHand = hand.get(0).value + hand.get(1).value;
+		if(bankerHand >= 7) {
+			return false;
+		}
+		else if(bankerHand == 0 || bankerHand == 1 || bankerHand == 2) {
+			return true;
+		}
+		else if(bankerHand == 3 || bankerHand == 4 || bankerHand == 5 || bankerHand == 6) {
+			if(bankerHand == 6 && (playerCard.value == 6 || playerCard.value == 7)) {
 				return true;
+			}
+			else if(bankerHand == 5 && ((playerCard.value >= 4 && playerCard.value <= 7) 
+					|| evaluatePlayerDraw(hand) == false)) {
+				return true;
+			}
+			else if(bankerHand == 4 && ((playerCard.value >= 2 && playerCard.value <= 7)
+					|| (evaluatePlayerDraw(hand) == false))) {
+				return true;
+			}
+			else if(bankerHand == 3 && (playerCard.value != 8 || evaluatePlayerDraw(hand) == false)) {
+				return true;
+			}
+			else {
+				return false;
 			}
 		}
 		return false;
 	}
 	
 	public static boolean evaluatePlayerDraw(ArrayList<Card> hand) {
+		int playerHand = hand.get(0).value + hand.get(1).value;
+		if(playerHand >= 6) {
+			return false;
+		}
+		else if(playerHand <= 5) {
+			return true;
+		}
 		return false;
 	}
 }
